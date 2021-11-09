d6b5c2d1bc32686b47aa2bb3b54a0be8429e6071
Félix Bal <felixbal73@gmail.com>
2021-11-09 10:40:08 +0100

    testV

---
+++ felix/leaflet.js
@@ -0,0 +1,41 @@
+var mymap = L.map('map', {
+    zoomSnap: 0.8
+}).setView([51.505, 0], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+
+// on ajoute un élément sur la carte
+var myIcon = L.icon({
+  iconUrl: 'takieddine.jpg',
+  iconSize: [45, 45],
+  popupAnchor: [0, -20]
+});
+
+L.marker([51.5, 0], {icon: myIcon}).addTo(mymap)
+  .bindPopup('Je suis takieddine');
+
+
+// on ajoute l'inventaire en bas à gauche
+L.Control.Watermark = L.Control.extend({
+  onAdd: function(mymap) {
+    var img = L.DomUtil.create('img');
+
+    img.src = 'inventaire.png';
+    img.style.width = '350px';
+
+    return img;
+  },
+
+  onRemove: function(mymap) {
+      // Nothing to do here
+      }
+  });
+
+  L.control.watermark = function(opts) {
+      return new L.Control.Watermark(opts);
+  }
+
+  L.control.watermark({ position: 'bottomleft' }).addTo(mymap);
