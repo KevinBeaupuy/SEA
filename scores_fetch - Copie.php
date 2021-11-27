747c34ac5d7aead75aafec05f5a6639d161caac9
KevinBeaupuy <kevb1899@gmail.com>
2021-11-27 02:05:05 +0100

    rgztghzrthztgz

---
+++ scores_fetch - Copie.php
@@ -0,0 +1,31 @@
+<?php
+//importation de la bdd
+include('connect.php');
+
+  $requete2 = "SELECT * FROM scores ORDER BY score ASC";
+  $tab=[];
+  if($result=mysqli_query($link,$requete2)){
+    while($ligne=mysqli_fetch_assoc($result)){
+      //tableau associatif
+      $tab[]=$ligne;
+     }
+   }
+
+   //rédaction du tableau des scores
+   $out = "<table><tr>";
+
+   foreach($tab[0] as $key => $elem){
+     $out .= "<th>".$key."</th>";
+   }
+
+   foreach($tab as $onegens){
+     $out .= "</tr><tr>";
+     foreach($onegens as $key => $elem){
+       $out .= "<td>".$elem."</td>";
+     }
+   }
+   $out .= "</tr></table>";
+
+   // affichage
+   echo json_encode($out);
+?>
