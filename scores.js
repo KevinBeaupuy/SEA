b18841cfbb39ef78c37bb4bceef2789a5bbb97d7
KevinBeaupuy <kevb1899@gmail.com>
2021-11-28 16:28:10 +0100

    test push

--- scores.js
+++ scores.js
@@ -7,16 +7,31 @@
 // });
 
 
+console.log(location.pathname);
+if (location.pathname == "/game.html") {
+  $.ajax({
+    url: "scores_fetch_best.php",
+    type: "POST",
+    dataType: "json",
+    data: {}
+  })
+    .done(function( data,status ) {
 
-$.ajax({
-  url: "scores_fetch.php",
-  type: "POST",
-  dataType: "json",
-  data: {}
-   // text: $("p#tab_score").text()
-})
-  .done(function( data,status ) {
-    
-    //Selecteur du tableau html
-    $("#tab_score").html(data);
-  });
+      //Selecteur du tableau html
+      $("#tab_score").html(data);
+    });
+}
+
+else {
+  $.ajax({
+    url: "scores_fetch.php",
+    type: "POST",
+    dataType: "json",
+    data: {}
+  })
+    .done(function( data,status ) {
+
+      //Selecteur du tableau html
+      $("#tab_score").html(data);
+    });
+}
